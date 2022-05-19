ba23e240e8e4ebec9e6c901f55a028d8a0da1786
yvoatelep <yvoatelep@gmail.com>
2022-05-19 09:12:06 -0600

    module 9

--- app.py
+++ app.py
@@ -11,17 +11,45 @@ Base = automap_base()
 Base.prepare(engine, reflect=True)
 Measurement = Base.classes.measurement
 Station = Base.classes.station
-session = Session(engine)
+
 app = Flask(__name__)
 @app.route("/")
 def welcome():
+    print("Server received request for 'Home' page...")
     return(
-    '''
-    Welcome to the Climate Analysis API!
+    f'''
+    Welcome to the Climate Analysis API!test
     Available Routes:
     /api/v1.0/precipitation
     /api/v1.0/stations
     /api/v1.0/tobs
     /api/v1.0/temp/start/end
     ''')
-NOTE
+    
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    session = Session(engine)
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= prev_year).all()
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
+@app.route("/api/v1.0/tobs")
+def temp_monthly():
+    session = Session(engine)
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).\
+      filter(Measurement.station == 'USC00519281').\
+      filter(Measurement.date >= prev_year).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
+
+if __name__ == '__main__':
+    app.run(debug=True)
